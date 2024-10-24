0bc008fdd5035b4ee2bc637ee17ef1bfa1d23baf
baid <cvscholtisek@alaska.edu>
2024-10-23 20:52:10 -0800

    Timer mode/Infinite mode/Updated Word lists
    - Added a time limit mode with a customizable time limit variable before the game automatically ends
    - Added an infinite mode as a sub-mode from time limit mode, where it functions exactly like time limit mode but when you guess the word right, the game adds one score, resets the board, finds a new word, and adds a customizable amount of time to the time limit. You can also customize the word length and amount of guesses like normal.
    - Recreated the word lists for 3, 4, 6, 7 letter word sizes to be more robust using a custom SCOWL (spell checker oriented word lists) word list http://wordlist.aspell.net/ This word list should include:
    "all the strange and unusual words people like to use in word games such as Scrabble (TM).  While a lot of the words in the 80 size are not used very often, they are all generally considered valid words in the English language"
    I made a small python script to filter out the words into different files of appropriate word lengths and to filter out words with punctuation. I didn't test it too extensively but I didn't have any issue with real english words being "invalid" anymore
    
    This word list is used for checking word validation. The solution words are chosen from a separate list using much more common words that most people should be familiar with, making it easier to guess
    Solution list word samples:
    analog ask attract blame books calculate cease children circuit dates
     discover hardest help loaded obviously organized private proof somehow
     sorts spelled spot vision water writing
    Validation list word samples:
    abomas ambulators bethanking cointer epeeist grammaticalnesses meshier
     mischanneled nonparochial obo oulakan outserving overcanopying paralalia
     pinschers pleromas reshines sarcosaprophagous speedskating swotters teade
     thoughtsick unplacing unsweet wayback
    
    - After the game ends (running out of time or guesses) the target word will display showing what the word was before starting a new game.
    - Refactored the keyboard layout to be a little more uniform
    - Changed a bunch of UI anchors and such to work better on different screen sizes

---
+++ Assets/scripts/CS_Timer.cs
@@ -0,0 +1,91 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using JetBrains.Annotations;
+
+public class CS_Timer : MonoBehaviour
+{
+    [SerializeField]
+    private Board Game;
+
+    [Header("Timer")]
+    [SerializeField]
+    private TextMeshProUGUI timerText;
+    [Tooltip("Should a countdown time limit be enforced")]
+    public bool timerEnabled;
+    [Tooltip("Time limit in seconds")]
+    public int timeLimit;
+    [Tooltip("On correct guess, should a new word be found and time be added? (This overrides timerEnabled boolean)")]
+    public bool infiniteMode;
+    [Tooltip("If infinite mode is enabled, how much time should be added on correct guess in seconds")]
+    public float correctGuessTimeReward;
+
+    private float timer;
+    private bool isTimerRunning;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(isTimerRunning)
+            IncrementTimer();
+    }
+
+    public void ResetTimer()
+    {
+        timer = 0;
+    }
+
+    public void StartTimer()
+    {
+        isTimerRunning = true;
+    }
+
+    public void StopTimer()
+    {
+        isTimerRunning = false;
+    }
+
+    public void AddTime(float time)
+    {
+        timer -= time;
+    }
+
+    public void PauseTime(float pauseDuration)
+    {
+
+    }
+
+    public void IncrementTimer()
+    {
+        TimeSpan time;
+        // increment timer
+        timer += Time.deltaTime;
+
+            
+        // check if time limit is over
+        if (timerEnabled)
+        {
+            if (timer >= timeLimit)
+                Game.GameOver();
+            time = TimeSpan.FromSeconds(timeLimit - timer);
+            timerText.text = time.ToString(@"mm\:ss");
+
+            if (time.TotalSeconds < 60 && timerText.color != Color.red)
+            {
+                    timerText.color = Color.red;
+            }
+            else if (timerText.color != Color.white && time.TotalSeconds > 60)
+            {
+                timerText.color = Color.white;
+            }
+        }
+        // if a time limit is not enforced (stopwatch mode)
+        else
+        {
+            time = TimeSpan.FromSeconds(timer);
+        }
+        timerText.text = time.ToString(@"mm\:ss");
+    }
+}
