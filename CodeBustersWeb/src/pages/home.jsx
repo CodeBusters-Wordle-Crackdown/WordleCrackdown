cbda87cfd90425d080496f8d4f14d72e6782eea4
Brian <55475432+Arthurgamer1@users.noreply.github.com>
2024-10-28 19:45:46 -0800

    Web dev front (#38)
    * Create deploy_dev.yml
    
    * Create deploy_test.yml
    
    * Create deploy_prod.yml
    
    * deploy.yml
    
    Deployment pipelines
    
    * Removed-deploy
    
    Removed, moved to environment
    
    * REMOVED
    
    This file is to be removed
    
    * Rename deploy_prod to REMOVED(2)
    
    * Updated deploy.yml
    
    * 18 Sep 24 - CSCE 401 Project Plan
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * -changed hyperlink to local version of game
    
    * updated hyperlink to test game
    
    * Update .gitignore
    
    * Update .gitignore
    
    * Update .gitignore
    
    * Update deploy.yml
    
    * Rename deploy.yml to firebase-hosting.yml
    
    * Update firebase-hosting.yml
    
    * Update firebase-hosting.yml
    
    * making a branch with a common ancestor
    
    * uploading front end files
    
    * update readme and trying to link to firebase
    
    * Major visual parallax change to start focusing looking like wordle
    
    * deleted unecessary duplicate files in the last folder that pertain to react, and installed those dependencies inside the react folder
    
    * Firebase SDK added & WebGL not working once contructed... working on a fix
    
    ---------
    
    Co-authored-by: girlReWired <63616817+girlReWired@users.noreply.github.com>
    Co-authored-by: cehinds <153137703+cehinds@users.noreply.github.com>

---
+++ CodeBustersWeb/src/pages/home.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import Header from '../components/Header';
+import Footer from '../components/footer';
+import '../App.css';
+import { Parallax, ParallaxLayer } from '@react-spring/parallax';
+
+function Home() {
+    return (
+        <div>
+
+            {/*Parallax Effect*/}
+            <Parallax pages={1.5} style={{ top: '0', left: '0' }} className="animation">
+
+                <ParallaxLayer offset={0}>
+                    <Header />
+                </ParallaxLayer>
+
+
+                <ParallaxLayer offset={0.12}>
+                    <div className='animation_lp' id='artback'></div>
+                </ParallaxLayer>
+
+                <ParallaxLayer offset={0.80} speed={0.39}>
+                    <div className='Par' id='Layer2'>
+                        <h1>Lorem ipsum</h1> <br />
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam ac gravida nunc,
+                            et blandit ligula. Etiam turpis massa, pharetra imperdiet ultrices a, aliquam et
+                            diam. Curabitur congue nibh sed metus dignissim, eu auctor erat lobortis. Nulla
+                            gravida purus sed bibendum ultrices. Aliquam sed feugiat eros, nec elementum quam.
+                            Donec vitae leo dignissim, efficitur mi vitae, placerat sapien. Quisque maximus,
+                            est quis lacinia dapibus, sapien est consequat justo, ut faucibus lectus tellus eu
+                            tortor. Suspendisse maximus justo dolor, id finibus turpis fringilla vehicula.
+                            Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia
+                            curae; Phasellus lorem ipsum, aliquam vitae viverra sagittis, eleifend id augue.
+                        </p>
+                    </div>
+                </ParallaxLayer>
+
+                <ParallaxLayer offset={0.95}>
+                    <Footer />
+                </ParallaxLayer>
+            </Parallax>
+
+        </div>
+    );
+}
+
+export default Home;
