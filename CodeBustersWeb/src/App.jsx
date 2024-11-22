039b680e63bd5fa4d02461ffcc7e64521fe73278
girlReWired <63616817+girlReWired@users.noreply.github.com>
2024-11-22 14:14:43 -0900

    Fixing merge issues

--- CodeBustersWeb/src/App.jsx
+++ CodeBustersWeb/src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/home";
+import Home from "./pages/home"; // Matches the actual file name "home.jsx"
 import Game from "./pages/Game";
 
 function App() {
